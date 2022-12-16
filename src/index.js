ff8a5d4a23837f5bf7cea8264786b56d682f64c1
panley01 <52224740+panley01@users.noreply.github.com>
2022-12-16 19:35:08 +0000

    Added compliance with Discord's bad domains list
    This list near exclusively contains known bad phishing domains. This is very baseline moderation that I would be uncomfortable without. You are free to remove this.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ export default {
 		if (!_URL.startsWith('https://')) {
 			if (!_URL.startsWith('http://')) {_URL = `https://${_URL}`}
 		}
-		const r = await fetch(_URL)
+		const parsedURL = new URL(_URL)
+		let r = await fetch('https://cdn.discordapp.com/bad-domains/updated_hashes.json')
+		const badDomains = await r.json()
+		const encoder = new TextEncoder()
+		const domainHash = Array.from(new Uint8Array(await crypto.subtle.digest("SHA-256", encoder.encode(parsedURL.hostname)))).map((b) => b.toString(16).padStart(2, '0')).join('')
+		if (badDomains.includes(domainHash)) return Response.redirect('https://twitter.com/panley01/status/1603834209646280721')
+		r = await fetch(_URL)
 		const body = await r.text()
-		const responseBody = `<!DOCTYPE html>\n<head>${(body.split('<head>')[1]).split('</head>')[0]}<meta http-equiv="refresh" content="2;url=${_URL}" />\n</head>`
+		const responseBody = `<!DOCTYPE html>\n<head>${(body.split('<head>')[1]).split('</head>')[0]}<meta http-equiv="refresh" content="2;url=${_URL}" />\n</head>\n<body>\n<img src="https://http.cat/301">\n</body>`
 		if (REQUESTURL.searchParams.get('dev')) return new Response(responseBody)
 		return new Response(responseBody, {headers: {'content-type': 'text/html; charset=utf-8'}})
 	}
